3bdc9db5defddba76370cb4dbfcfbe4b407ac142
Fernando Schuindt <f.schuindtcs@gmail.com>
2019-03-31 15:14:14 -0300

    chore: Configure PHP 'max_input_time'

--- configure_php.sh
+++ configure_php.sh
@@ -15,6 +15,7 @@ sed -i "s|;*upload_max_filesize =.*|upload_max_filesize = ${PHP_MAX_UPLOAD}|i" /
 sed -i "s|;*max_file_uploads =.*|max_file_uploads = ${PHP_MAX_FILE_UPLOAD}|i" /etc/php7/php.ini
 sed -i "s|;*post_max_size =.*|post_max_size = ${PHP_MAX_POST}|i" /etc/php7/php.ini
 sed -i "s|;*max_execution_time =.*|max_execution_time = ${PHP_MAX_EXECUTION_TIME}|i" /etc/php7/php.ini
+sed -i "s|;*max_input_time =.*|max_input_time = ${PHP_MAX_INPUT_TIME}|i" /etc/php7/php.ini
 sed -i "s|;*cgi.fix_pathinfo=.*|cgi.fix_pathinfo= ${PHP_CGI_FIX_PATHINFO}|i" /etc/php7/php.ini
 sed -i "s|;*auto_prepend_file =.*|auto_prepend_file = /etc/php7/rpaf.php|i" /etc/php7/php.ini
 
